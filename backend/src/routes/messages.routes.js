ce08a3f0fe1d7aba2d4b1c811f0c40a9a7149ab2
= <=>
2026-03-06 19:26:45 +0530

    Implement the sendMessage API endpoint with targetFolderConstants, Cloudinary Storage, and Multer Storage.

--- backend/src/routes/messages.routes.js
+++ backend/src/routes/messages.routes.js
@@ -1,10 +1,13 @@
 import { authRouteMiddleware } from "../middlewares/auth.middleware.js";
 import express from "express";
+import multer from "multer";
 import { getMessages, getUsers, sendMessage } from "../controllers/message.controller.js";
 const route = express.Router();
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 route.get("/users", authRouteMiddleware, getUsers);
 route.get("/:id", authRouteMiddleware, getMessages);
-route.post("/send/:id", authRouteMiddleware, sendMessage);
+route.post("/send/:id", authRouteMiddleware, upload.single("media"), sendMessage);
 
 export default route;
