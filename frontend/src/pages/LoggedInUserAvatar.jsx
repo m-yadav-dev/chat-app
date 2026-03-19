450605acf1c4e7c00d3dc9d3426d3f7cbb09ce35
= <=>
2026-03-19 10:10:03 +0530

    test workflow

--- frontend/src/pages/LoggedInUserAvatar.jsx
+++ frontend/src/pages/LoggedInUserAvatar.jsx
@@ -4,11 +4,11 @@ const LoggedInUserAvatar = ({ selectedImage, user }) => {
 //   const profilePic = user?.profilePic || "";
   const fallbackAvatar = fullName ? fullName.charAt(0) : "";
   return (
-    <Avatar className="w-full h-full bg-slate-700 text-slate-200" size="lg">
+    <Avatar className=" bg-slate-700 text-slate-200">
       <AvatarImage
         src={selectedImage || undefined}
         alt="profile"
-        className="w-full h-full object-cover"
+        className="w-full h-full object-cover "
       />
       <AvatarFallback className="w-full h-full flex items-center justify-center">{fallbackAvatar.toUpperCase()}</AvatarFallback>
     </Avatar>
