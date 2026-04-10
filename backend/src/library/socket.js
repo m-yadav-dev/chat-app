8a9940db74010cdcf934d75096db7decf70fa571
= <=>
2026-04-10 15:13:02 +0530

    Configure deployment, implement CORS, dynamic ports and env variables

--- backend/src/library/socket.js
+++ backend/src/library/socket.js
@@ -3,15 +3,15 @@ import http from "http";
 import express from "express";
 import { ENV_VARS } from "./env.js";
 import { socketAuthMiddleware } from "../middlewares/socket.middleware.js";
-
 const app = express();
+const server = http.createServer(app);
+
 const socketAllowedOrigins = [
   ENV_VARS.CLIENT_URL,
   "http://localhost:5173",
   "http://localhost:5174",
 ].filter(Boolean);
 
-const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
